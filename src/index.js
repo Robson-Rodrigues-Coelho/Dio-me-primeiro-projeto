f47c0cac73e0eb57777b3734188e414192c815ff
Robson-Rodrigues-Coelho <robson.empreendimentos@gmail.com>
2023-11-07 00:43:16 -0300

    primeiros erros

---
+++ src/index.js
@@ -0,0 +1,13 @@
+let heroi = "Felipão"
+let level = 1000
+
+switch (level) {
+
+    case level > 0 && level <= 1000
+    console.log("O herói de nome " + heroi + " está no nível Ferro.");
+        break;
+
+
+
+}
+
